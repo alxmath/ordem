6374472b467a7d2c370dac8ba8d487e5f4aed270
Alex Santos <alxmath@gmail.com>
2022-10-26 20:47:48 -0300

    Traduzindo o Datatable da view usuarios

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -19,4 +19,37 @@ class Usuarios extends BaseController
 
         return view('Usuarios/index', $data);
     }
+
+    public function recuperaUsuarios()
+    {
+        if (!$this->request->isAJAX())
+        {
+            return redirect()->back();
+        }
+
+        $atributos = [
+            'id', 'nome', 'email', 'ativo', 'imagem'
+        ];
+
+        $usuarios = $this->usuarioModel->select($atributos)
+            ->findAll();
+
+        $data = [];
+
+        foreach ($usuarios as $usuario)
+        {
+            $data[] = [
+                'imagem' => $usuario->imagem,
+                 'nome' => esc($usuario->nome),
+                 'email' => esc($usuario->email),
+                 'ativo' => $usuario->ativo == true ? 'Ativo' : '<span class="text-warning">Inativo</span>',
+            ];
+        }
+
+        $retorno = [
+            'data' => $data
+        ];
+
+        return $this->response->setJSON($retorno);
+    }
 }
--- app/Views/Usuarios/index.php
+++ app/Views/Usuarios/index.php
@@ -31,4 +31,64 @@
 
 <?= $this->section('scripts') ?>
 <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/r-2.3.0/datatables.min.js"></script>
+
+<script>
+  $(document).ready(function() {
+
+    const DATATABLE_PTBR = {
+      "sEmptyTable": "Nenhum registro encontrado",
+      "sInfo": "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+      "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+      "sInfoFiltered": "(Filtrados de _MAX_ registros)",
+      "sInfoPostFix": "",
+      "sInfoThousands": ".",
+      "sLengthMenu": "_MENU_ resultados por página",
+      "sLoadingRecords": "Carregando...",
+      "sProcessing": "Processando...",
+      "sZeroRecords": "Nenhum registro encontrado",
+      "sSearch": "Pesquisar",
+      "oPaginate": {
+        "sNext": "Próximo",
+        "sPrevious": "Anterior",
+        "sFirst": "Primeiro",
+        "sLast": "Último"
+      },
+      "oAria": {
+        "sSortAscending": ": Ordenar colunas de forma ascendente",
+        "sSortDescending": ": Ordenar colunas de forma descendente"
+      },
+      "select": {
+        "rows": {
+          "_": "Selecionado %d linhas",
+          "0": "Nenhuma linha selecionada",
+          "1": "Selecionado 1 linha"
+        }
+      }
+    }
+
+
+    $('#ajaxTable').DataTable({
+      "oLanguage":DATATABLE_PTBR,
+      'ajax': '<?php echo site_url('usuarios/recuperausuarios'); ?>',
+      'columns': [{
+          'data': 'imagem'
+        },
+        {
+          'data': 'nome'
+        },
+        {
+          'data': 'email'
+        },
+        {
+          'data': 'ativo'
+        },
+      ],
+      "deferRender": true,
+      "processing": true,
+      "responsive": true,
+      "pagingType": $(window).width() < 768 ? "simple" : "simple_numbers",
+    });
+  });
+</script>
+
 <?= $this->endsection() ?>
